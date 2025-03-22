90913de7c4ddeb929ef0fc9f3663c54f2b3b84ff
Julien Duseyau <julien.duseyau@gmail.com>
2025-03-23 00:21:57 +0100

    Implement basic ContainerCompiler

---
+++ tests/integration/Compiler/Mocks/Logger.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Tests\Integration\Compiler\Mocks;
+
+class Logger {
+    public function log(string $msg): void {}
+}
---
+++ tests/integration/Compiler/Mocks/Mailer.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests\Integration\Compiler\Mocks;
+
+class Mailer {
+    public Logger $logger;
+
+    public function __construct(Logger $logger)
+    {
+        $this->logger = $logger;
+    }
+}
