26f1865d1431ed4b4c04f8bf75a5884fe4c8c89f
Julien Duseyau <julien.duseyau@gmail.com>
2025-10-25 09:29:33 +0200

    Refactor container structure by renaming files and updating autoload paths

--- src/Interceptors/Post/InitInterceptor.php
+++ src/Interceptors/Post/InitInterceptor.php
@@ -6,20 +6,21 @@ namespace Maduser\Argon\Container\Interceptors\Post;
 
 use Maduser\Argon\Container\Contracts\PostResolutionInterceptorInterface;
 use Maduser\Argon\Container\Interceptors\Post\Contracts\InitInterface;
+use Override;
 
 /**
  * Intercepts resolved instances implementing InitInterface and triggers their init() method.
  */
 final readonly class InitInterceptor implements PostResolutionInterceptorInterface
 {
-    #[\Override]
+    #[Override]
     public static function supports(object|string $target): bool
     {
         return $target instanceof InitInterface
             || (is_string($target) && is_subclass_of($target, InitInterface::class));
     }
 
-    #[\Override]
+    #[Override]
     public function intercept(object $instance): void
     {
         if ($instance instanceof InitInterface) {
